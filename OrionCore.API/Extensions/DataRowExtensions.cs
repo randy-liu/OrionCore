e7781436e142bd065e70d1e6bd58b5008a0cd78c
Jax Hu <weskerjax@gmail.com>
2021-11-18 16:45:13 +0800

    [^] 針對 DataRow ToJson 做轉換處理

---
+++ OrionCore.API/Extensions/DataRowExtensions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Orion.Api.Extensions
+{
+    /// <summary>定義 DataRow 的 Extension</summary>
+    public static class DataRowExtensions
+    {
+
+        public static Dictionary<string, object> ToDictionary(this DataRow row)
+        {
+            if (row == null) { return null; }
+
+            var dict = new Dictionary<string, object>();
+
+            foreach (DataColumn col in row.Table.Columns)
+            { dict[col.ColumnName] = row[col]; }
+
+            return dict;
+        }
+
+
+
+
+        /*##############################################################*/
+
+        private static T toType<T>(DataRow row, string columnName, T empty)
+        {
+            if (row == null) { return empty; }
+
+            object value = row[columnName];
+            if (value == null) { return empty; }
+            if (value is T tValue) { return tValue; }
+
+            return value.ConvertTo<T>();
+        }
+
+
+        public static string String(this DataRow row, string columnName)
+        {
+            return toType<string>(row, columnName, null);
+        }
+
+
+        public static int Int(this DataRow row, string columnName)
+        {
+            return toType<int>(row, columnName, 0);
+        }
+
+        public static double Double(this DataRow row, string columnName)
+        {
+            return toType<double>(row, columnName, 0d);
+        }
+
+        public static decimal Decimal(this DataRow row, string columnName)
+        {
+            return toType<decimal>(row, columnName, 0m);
+        }
+
+        public static DateTime DateTime(this DataRow row, string columnName)
+        {
+            return toType<DateTime>(row, columnName, System.DateTime.MinValue);
+        }
+
+        public static DateTime? DateTimeN(this DataRow row, string columnName)
+        {
+            return toType<DateTime?>(row, columnName, null);
+        }
+
+
+        public static bool ContainsColumn(this DataRow row, string column)
+        {
+            if (row == null) { return false; }
+            return row.Table.Columns.Contains(column);
+        }
+
+
+
+
+
+
+
+    }
+
+}
--- OrionCore.API/Extensions/DataTableExtensions.cs
+++ OrionCore.API/Extensions/DataTableExtensions.cs
@@ -9,7 +9,6 @@ namespace Orion.Api.Extensions
 	public static class DataTableExtensions
 	{
 
-
         public static Dictionary<TKey, TElement> ToDictionary<TKey, TElement>(this DataTable table, Func<DataRow, TKey> keySelector, Func<DataRow, TElement> elementSelector)
         {
             if (table == null) { return new Dictionary<TKey, TElement>(); }
@@ -60,72 +59,6 @@ namespace Orion.Api.Extensions
 
 
 
-        private static T toType<T>(this DataRow row, string columnName, T empty)
-        {
-            if (row == null) { return empty; }
-
-            object value = row[columnName];
-            if (value == null) { return empty; }
-            if (value is T tValue) { return tValue; }
-
-            return value.ConvertTo<T>();
-        }
-
-
-        public static string String(this DataRow row, string columnName)
-        {
-            return toType<string>(row, columnName, null);
-        }
-
-
-        public static int Int(this DataRow row, string columnName)
-        {
-            return toType<int>(row, columnName, 0);
-        }
-
-        public static double Double(this DataRow row, string columnName)
-        {
-            return toType<double>(row, columnName, 0d);
-        }
-
-        public static decimal Decimal(this DataRow row, string columnName)
-        {
-            return toType<decimal>(row, columnName, 0m);
-        }
-
-        public static DateTime DateTime(this DataRow row, string columnName)
-        {
-            return toType<DateTime>(row, columnName, System.DateTime.MinValue);
-        }
-
-        public static DateTime? DateTimeN(this DataRow row, string columnName)
-        {
-            return toType<DateTime?>(row, columnName, null);
-        }
-
-
-        public static bool ContainsColumn(this DataRow row, string column)
-        {
-            if (row == null) { return false; }
-            return row.Table.Columns.Contains(column);
-        }
-
-
-
-        /// <summary>將 DataRow 轉換為 Json</summary>
-        public static string ToJson(this DataRow row)
-        {
-            var dict = new Dictionary<string, object>();
-
-            foreach (DataColumn col in row.Table.Columns)
-            { dict[col.ColumnName] = row[col]; }
-
-            return ObjectExtensions.ToJson(dict);
-        }
-
-
-
-
         /*##############################################################*/
 
         /// <summary>將 DataRow 與 POCO Mapping，回傳 IEnumerable of POCO</summary>
@@ -158,8 +91,6 @@ namespace Orion.Api.Extensions
                 return model;
             });
         }
-         
-
 
          
         /// <summary>將 IEnumerable of POCO 回傳 DataTable</summary>
@@ -192,15 +123,6 @@ namespace Orion.Api.Extensions
 
             return table;
         }
-
-
-
-
-
-
-
-
-
 
 
 
